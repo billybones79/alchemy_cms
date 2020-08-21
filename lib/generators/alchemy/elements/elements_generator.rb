76bfa6602156230f9b2f550e11a82d32062c1f05
LWS <alexandredt79@gmail.com>
2020-08-21 15:57:39 +0000

    des nouveaux fichiers??

--- lib/generators/alchemy/elements/elements_generator.rb
+++ lib/generators/alchemy/elements/elements_generator.rb
@@ -1,20 +1,24 @@
-# frozen_string_literal: true
-require_relative "../base"
+require_relative '../../../rails/generators/alchemy/base'
 
 module Alchemy
   module Generators
     class ElementsGenerator < Base
       desc "This generator generates your elements view partials."
-      source_root File.expand_path("templates", __dir__)
+      source_root File.expand_path('templates', __dir__)
 
       def create_partials
-        @elements = load_alchemy_yaml("elements.yml")
+        @elements = load_alchemy_yaml('elements.yml')
         return unless @elements
 
         @elements.each do |element|
           @element = element
           @contents = element["contents"] || []
-          @element_name = element_name(element)
+          if element["name"] =~ /\A[a-z0-9_-]+\z/
+            @element_name = element["name"].underscore
+          else
+            raise "Element name '#{element['name']}' has wrong format. Only lowercase and non whitespace characters allowed."
+          end
+
           conditional_template "view.html.#{template_engine}", "#{elements_dir}/_#{@element_name}.html.#{template_engine}"
         end
       end
@@ -23,14 +27,6 @@ module Alchemy
 
       def elements_dir
         @_elements_dir ||= "app/views/alchemy/elements"
-      end
-
-      def element_name(element)
-        if element["name"] =~ Alchemy::Element::NAME_REGEXP
-          element["name"].underscore
-        else
-          raise "Element name '#{element["name"]}' has wrong format. Only lowercase and non whitespace characters allowed."
-        end
       end
     end
   end
